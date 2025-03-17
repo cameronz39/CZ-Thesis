1c63653ff8c12b5763ea1d4fa931ccc132ccd3dc
cameronz39 <cameronzorio@gmail.com>
2025-03-16 17:47:54 -0700

    removed dependence on MATLAB functions in the Choi Simulink Model

--- Choi_SimulinkScript.m
+++ Choi_SimulinkScript.m
@@ -15,14 +15,17 @@ J_0 = [1.815  -0.014 0.004;
        0.004  0.008 1.475];
 
 % r_0 = [-6*10^-3, 7*10^-3 ,-3*10^-2]'; % solveable condition (displaced battery)
-r_0 = [6*10^-3, 0 ,-3*10^-2]'; % solveable condition (displaced battery)
+r_0 = [6*10^-3, 6*10^-3 ,-3*10^-2]'; % solveable condition (displaced battery)
 % r_0 = [0 0 0]';
 
-omega_0 = [0 0.0001 0.02]';
+omega_0 = [0 0.00 0.02]';
+% omega_0 = [0 0.00 0.00]';
 
 EA_0 = deg2rad([0 0 0]'); 
 
-q_0 = eul2quat(EA_0')'; % transformations are inertial to body DONT TRUST EUL2QUAT
+% transformations are inertial to body
+% XYZ rotation order must be explicitly specified
+q_0 = eul2quat(EA_0',"XYZ")'; 
 
 q_d = [1 0 0 0]';
 
@@ -50,6 +53,7 @@ t = out.tout;
 r_b = out.r_b.signals.values;
 r_mmus = out.r_mmus.signals.values;
 EA_b_N = out.EA_b_N.signals.values;
+q_b_N = out.q_b_N.signals.values;
 
 figure;
 plot(t,r_b,'LineWidth',1.2);
@@ -70,7 +74,8 @@ ylabel("Sliding Mass Positions")
 legend("x Mass","y Mass", "z Mass", "Travel Limit")
 
 figure;
-plot(t,EA_b_N,'LineWidth',1.2)
+% plot(t,EA_b_N,'LineWidth',1.2)
+plot(t,quat2eul(q_b_N,'XYZ'),'LineWidth',1.2)
 grid on
 legend("Roll","Pitch","Yaw")
 
