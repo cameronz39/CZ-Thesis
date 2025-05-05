270ff1cb6653c013e77000da2fe04ac2250ed71f
cameronz39 <cameronzorio@gmail.com>
2025-05-04 22:09:43 -0700

    simple UKF working with gravity torque

--- SADS_UKF_Script.m
+++ SADS_UKF_Script.m
@@ -7,14 +7,19 @@ clear
 
 tf = 50;          % simulation length [s]
 n  = 4;            % number of states
+alpha   = 1e-3;         % small, positive
+kappa   = 0;            % usually 0
+beta    = 2;            % optimal for Gaussian
+lambda  = alpha^2*(n+kappa) - n;
+
 
 % Test-bed characteristics 
 m_s = 33.629; % [kg]
-J = [1.815  -0.014 0.004;
-      -0.014  1.348  0.008;
-       0.004  0.008 1.475];
+J = [1.815  0 0;
+      0  1.348  0;
+       0  0 1.475];
 % r_0 = [0.1*10^-3, 0.1*10^-3 ,-1*10^-3]';
-r_0 = [0 0 0]';
+r_0 = [0 0 -0.001]';
 g_N = [0 0 -9.81];
 
 % for simulating plant ("truth")
@@ -33,22 +38,22 @@ sample_time = 1/sample_rate; % IMU sample period
 
 % Filter matrices
 R = diag([0.005 0.005 0.005].^2);      % measurement-noise covariance
-H = [eye(3)  zeros(3,4)];             % measurement matrix
+H = [eye(3)  zeros(3,5)];             % measurement matrix
 
-Q  = diag([5e-5 5e-5 5e-5 4e-12 4e-12 4e-12 4e-12]);
+Q  = diag([5e-5 5e-5 5e-5 4e-12 4e-12 4e-12 4e-12 5e-6]);
 
-omega_0 = [0 0.00 0.03]';
-omega_0 = [0.50 0.10 0.30]';
+omega_0 = [0 0.00 0.00]';
+% omega_0 = [0.50 0.10 0.30]';
 
-EA_0 = deg2rad([1 0 0]'); 
+EA_0 = deg2rad([10 0 0]'); 
 
 % transformations are inertial to body
 % XYZ rotation order must be explicitly specified
 q_0 = eul2quat(EA_0',"XYZ")'; 
 
-x_0 = [omega_0; q_0];
+x_0 = [omega_0; q_0; -0.001];
 n = size(x_0,1);
-P_0 = diag([1e-6 1e-6 1e-6 1e-6 1e-6 1e-6 1e-6]);
+P_0 = diag([1e-6 1e-6 1e-6 1e-6 1e-6 1e-6 1e-6 1e-6]);
 
 
 
